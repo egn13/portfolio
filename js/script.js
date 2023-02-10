29c8a124717a50821e45ba928ecedea122cc0ffc
egn13 <egn13@rambler.ru>
2023-02-10 16:25:14 +0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElement = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElement.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+const count = document.querySelectorAll('.skills__progress-count'),
+      line = document.querySelectorAll('.skills__progress-line span');
+
+count.forEach((item, i) => {
+    line[i].style.width = item.innerHTML;
+})
